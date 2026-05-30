1cd2a4a27713d1b030376d4c906f2078c0f60a33
s4019328 <S4019328@student.rmit.edu.au>
2026-05-30 18:50:02 +1000

    feat: added test file to test maven test

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestClass {
+
+    @Test
+    void TestFunction(){
+        boolean iAmTrue = true;
+        boolean iAmFalse = false;
+
+        assertTrue(iAmTrue);
+        assertFalse(iAmFalse);
+    }
+}
