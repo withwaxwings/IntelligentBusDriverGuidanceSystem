bd1e398a7f0d9ed926cba547cd48a00bf34cb57d
Neel Shisheer <s4094857@student.rmit.edu.au>
2026-06-01 16:59:45 +1000

    test commit

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class TestClass {
-
     @Test
     void TestFunction(){
         boolean iAmTrue = true;
